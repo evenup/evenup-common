976951f9b1b7d7b31bc7fb6991ed5e5cbd58d2fd
Justin Lambert <jlambert@eml.cc>
2013-08-01 06:08:36 -0600

    add ability to set root mail alias, root mount point

--- spec/classes/common_init_spec.rb
+++ spec/classes/common_init_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'common', :type => :class do
   let(:facts) { { :concat_basedir => '/var/lib/puppet/concat' } }
 
-  describe "Common class with no parameters, basic test" do
+  describe "no parameters" do
 
     it { should create_class('common') }
     [ 'bash-completion', 'iftop', 'iotop', 'lsof', 'man', 'openssh-clients', 'rsync', 'screen',
@@ -20,7 +20,33 @@ describe 'common', :type => :class do
     it { should contain_file('/etc/init/control-alt-delete.conf').with_ensure('absent') }
     it { should contain_file('/etc/sysconfig/init') }
     it { should contain_file('/etc/profile.d/ps1.sh') }
+    it { should contain_mailalias('root').with_ensure('absent') }
+    it { should_not contain_mount('/') }
+    it { should_not contain_beaver__stanza('/var/log/messages') }
+    it { should_not contain_beaver__stanza('/var/log/secure') }
+    it { should_not contain_beaver__stanza('/var/log/sudolog') }
+  end
 
+  describe 'root alias' do
+    let(:params) { { :root_mail => 'sucker@example.com' } }
+
+    it { should contain_mailalias('root').with_ensure('present').with_recipient('sucker@example.com') }
+    it { should contain_exec('common_newaliases') }
+  end
+
+  describe '/ mount' do
+    describe 'no device' do
+      let(:params) { { :root_fstype => 'ext4' } }
+      it { should_not contain_mount('/') }
+    end
+
+    describe 'no fstype' do
+      let(:params) { { :root_device => '/dev/sda' } }
+      it { should_not contain_mount('/') }
+    end
+
+    let(:params) { { :root_device => '/dev/sda', :root_fstype => 'ext4' } }
+    it { should contain_mount('/') }
   end
 
   describe 'beaver logging' do
